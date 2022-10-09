73d0cbe2cc849f646c441c3da40c1c655b378f65
Mohammad Javad Nikbakht <37978546+javadnikbakht@users.noreply.github.com>
2022-10-10 00:35:32 +0330

    urls addresses changed, README file updated (#6)
    * perf(urls): address of urls made shorter and changed
    
    * chore(readme): README.md file updated with some first importan instructions

--- mobile_app_version/urls.py
+++ mobile_app_version/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from mobile_app_version import views
 
 urlpatterns = [
-    re_path(r'apps/(?P<type>(android|ios|pwa))$', views.AppInfoView.as_view(), name='get_app_info'),
-    path('apps/version/latest', views.LatestAppVersion.as_view(), name="get_latest_app_version"),
+    path('latest', views.LatestAppVersion.as_view(), name="get_latest_app_version"),
+    re_path(r'info/(?P<type>(android|ios|pwa))$', views.AppInfoView.as_view(), name='get_app_info'),
 ]
